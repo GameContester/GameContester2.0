f444dbcfbf6d727f4e438c3f464d7aa0e8a11360
Void Hikka <oleh.skotar@gmail.com>
2016-03-06 13:14:05 +0200

    Database connect strings repair

--- GameContester.DataAccess/Migrations/201603061039488_Initial.Designer.cs
+++ GameContester.DataAccess/Migrations/201603061039488_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace GameContester.DataAccess.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class AddIsBannedForUser : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddIsBannedForUser));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201509011031597_AddIsBannedForUser"; }
+            get { return "201603061039488_Initial"; }
         }
         
         string IMigrationMetadata.Source
