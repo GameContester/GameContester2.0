fad26fe64d93990d740c31c9a5f3119a75ce47f4
Messering <messering@yandex.ru>
2016-03-18 23:22:42 +0200

    Debug avatar picture

---
+++ GameContester.Service/Controllers/ProfilePictureController.cs
@@ -0,0 +1,64 @@
+﻿using GameContester.Application.Services;
+using GameContester.Contracts.Services;
+using GameContester.DataAccess;
+using GameContester.DataAccess.Repository;
+using GameContester.Domain.Entities;
+using GameContester.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace GameContester.Service.Controllers
+{
+    [RoutePrefix("api/profilepicture")]
+    public class ProfilePictureController : BaseController
+    {
+        private readonly IImageService imageService;
+
+        public ProfilePictureController(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        //POST api/profilepicture
+        [HttpPost]
+        public async Task<HttpResponseMessage> AddImage()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            string root = HttpContext.Current.Server.MapPath("~/Resources");
+            var provider = new MultipartFormDataStreamProvider(root);
+            // Read the form data.
+            await Request.Content.ReadAsMultipartAsync(provider);
+            imageService.SaveImage(this.UserId, provider);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage GetImage(string id)
+        {
+            byte[] fileData = imageService.GetImage(id);
+            if (fileData == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            HttpResponseMessage Response = new HttpResponseMessage(HttpStatusCode.OK);
+            Response.Content = new ByteArrayContent(fileData);
+            Response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/*");
+            return Response;
+        }
+}
+}
